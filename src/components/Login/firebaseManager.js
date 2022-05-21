d41ab1e020e14e979de3d4ba651acb6f708a59c3
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2022-05-21 21:58:45 +0600

    git ignore

--- src/components/Login/firebaseManager.js
+++ src/components/Login/firebaseManager.js
@@ -1,36 +1,35 @@
 import * as  firebase from 'firebase/app';
 import { initializeApp } from 'firebase/app';
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import firebaseConfig from './firebase.config';
- 
-   
+import firebaseConfig from '../../firebase.config';
+
+
 export const initializeLoginFramework = () => {
   if (firebase.getApps.length === 0) {
     firebase.initializeApp(firebaseConfig);
   }
 }
 
-export const handelGoogleSignIn  = async () =>{
+export const handelGoogleSignIn = async () => {
   const provider = new GoogleAuthProvider();
-const auth = getAuth();
-try {
-  const result = await signInWithPopup(auth, provider);
-  const user = result.user;
-  const { email, displayName, photoURL } = user;
-  const signIndUser = {
-    isSignedIn: true,
-    name: displayName,
-    email: email,
-    url: photoURL,
-  }; 
-  sessionStorage.setItem('loggedInUser', JSON.stringify(signIndUser));
-  return signIndUser;
-} catch (error) {
-  console.log(error);
+  const auth = getAuth();
+  try {
+    const result = await signInWithPopup(auth, provider);
+    const user = result.user;
+    const { email, displayName, photoURL } = user;
+    const signIndUser = {
+      isSignedIn: true,
+      name: displayName,
+      email: email,
+      url: photoURL,
+    };
+    sessionStorage.setItem('loggedInUser', JSON.stringify(signIndUser));
+    return signIndUser;
+  } catch (error) {
+    console.log(error);
+  }
 }
-}
 
 
 
 
-  
