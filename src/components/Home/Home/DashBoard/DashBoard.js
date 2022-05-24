0980166a366e23c30c460bd96e5ef275bf6cedf3
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2022-05-25 02:33:13 +0600

    chat render in ui

--- src/components/Home/Home/DashBoard/DashBoard.js
+++ src/components/Home/Home/DashBoard/DashBoard.js
@@ -10,60 +10,69 @@ import { Link } from 'react-router-dom';
 import { UserContext } from '../../../../App';
 
 import SocketIO from 'socket.io-client';
-const ENDPOINT = 'http://192.168.0.108:4500' ;
+import Message from './Message/Message';
+
+import ReactScrollToBottom from 'react-scroll-to-bottom';
+const ENDPOINT = 'http://192.168.0.108:4500';
 // const ENDPOINT2 = 'http://localhost:4500/' ;
 let socket;
 
 const DashBoard = () => {
     const [loggedInUser] = useContext(UserContext);
-    const { name, url } = loggedInUser; 
-    const [id,setId]= useState('');
-   
+    const { name, url } = loggedInUser;
+    const [id, setId] = useState('');
+    const [messages, setMessages] = useState([]);
+
     useEffect(() => {
-         socket = SocketIO(ENDPOINT, { transports: ['websocket'] });
+        socket = SocketIO(ENDPOINT, { transports: ['websocket'] });
         socket.on('connect', () => {
             console.log('connected');
             setId(socket.id)
         })
         console.log(socket)
 
-        socket.emit('joined',{loggedInUser})
+        socket.emit('joined', { loggedInUser })
 
-        socket.on('welcome',(data)=>{
-            console.log(data.user,data.message);
+        socket.on('welcome', (data) => {
+            console.log(data.user, data.message);
+            setMessages([...messages,data]);
         })
-        socket.on('userJoined',(data)=>{
-            console.log(data.user,data.message);
+        socket.on('userJoined', (data) => {
+            console.log(data.user, data.message);
+            setMessages([...messages,data]);
         })
-        socket.on('userLeft',(data)=>{
-            console.log(data.user,data.message);
+        socket.on('leave', (data) => {
+            console.log(data.user, data.message);
+            setMessages([...messages,data]);
         })
         return () => {
-           socket.emit('disconnected');
-           socket.off();
+            socket.emit('disconnected');
+            socket.off();
         };
-    }, [loggedInUser] );
+    }, [loggedInUser]);
 
     useEffect(() => {
-        socket.on('sendMessage',(data)=>{
-            console.log(data.user,data.message,data.id);
+        socket.on('sendMessage', (data) => {
+            console.log(data.user, data.message, data.id);
+            data.id===id? data.self=true:data.self=false;
+            setMessages([...messages,data]);
         });
 
         return () => {
-             
+            socket.off()
+
         };
-    }, );
+    },[messages]);
 
 
-    const sendMessage=()=>{
+    const sendMessage = () => {
 
-       const message=document.getElementById('txtMessage').value;
-
-        socket.emit('message',{message,id})
-        document.getElementById('txtMessage').value=''
+        const message = document.getElementById('txtMessage').value;
+        socket.emit('message', { message, id })
+        document.getElementById('txtMessage').value = ''
     }
-  
-    
+
+
 
     return (
         <div className='Dashboard row'>
@@ -133,60 +142,20 @@ const DashBoard = () => {
                             <h3>:</h3>
                         </div>
                         <div className="messageCard">
-                            <div className="messageBody">
+                            <ReactScrollToBottom className="messageBody">
                                 <div className="lastSeen d-flex justify-content-between align-items-center">
                                     <hr /><p className='text-main'>Today</p><hr />
                                 </div>
-                                
-                                <div className="senderContainer">
-                                    <div className="senderCard">
-                                        <div className="senderBody">
-                                            <img src={userAnna} className="m-1  align-self-end" width="40px" alt="" />
-                                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto odit corrupti nostrum quaerat facilis unde?</p>
-                                            <h2 className='text-main align-self-end'>...</h2>
-                                        </div>
-                                        <p>10:34 PM</p>
-                                    </div>
-                                </div>
 
-                                <div className="senderContainer">
-                                    <div className="senderCard">
-                                        <div className="senderBody">
-                                            <img src={userAnna} className="m-1  align-self-end" width="40px" alt="" />
-                                            <p>Lorem ips unde?</p>
-                                            <h2 className='text-main align-self-end'>...</h2>
-                                        </div>
-                                        <p>10:34 PM</p>
-                                    </div>
-                                </div>
+                                {
+                                    messages.map((item,i)=>  <Message key={i} data={item}></Message>)
+                                } 
 
-                                <div className="receiverContainer">
-                                    <div className="receiverCard">
-                                        <div className="receiverBody">
-                                            <h2 className='text-main align-self-end'>...</h2>
-                                            <p> uptatum!</p>
-                                            <img src={url} className="m-1  align-self-end" width="40px" alt="" />
-                                        </div>
-                                        <p>10:34 PM</p>
-                                    </div>
-                                </div>
-                                <div className="receiverContainer">
-                                    <div className="receiverCard">
-                                        <div className="receiverBody">
-                                            <h2 className='text-main align-self-end'>...</h2>
-                                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis quaerat corporis eius nesciunt alias voluptatum!</p>
-                                            <img src={url} className="m-1 align-self-end " width="40px" alt="" />
-                                        </div>
-                                        <p>10:34 PM</p>
-                                    </div>
-                                </div>
-
-
-                            </div>
+                            </ReactScrollToBottom>
                             <div className="sendCard">
                                 <textarea placeholder='text message' cols="40" rows="1" id='txtMessage' className='messageText'></textarea>
                                 <FontAwesomeIcon className='fileIcon ' icon={faPaperclip} />
-                                <FontAwesomeIcon onClick={()=>{sendMessage()}} className='sendIcon' icon={faPaperPlane} />
+                                <FontAwesomeIcon onClick={() => { sendMessage() }} className='sendIcon' icon={faPaperPlane} />
                             </div>
                         </div>
                     </div>
---
+++ src/components/Home/Home/DashBoard/Message/Message.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './Message.css'
+import userAnna from '../../../../../images/anna-morgan.jpg'
+import userIMG from '../../../../../images/user-siam.jpg'
+
+const Message = (props) => {
+    const { user, message, self } = props.data; 
+    return (
+        <div className="message">
+            {self ?
+                <div className="userContainer">
+                    <div className="userCard">
+                        <div className="userBody">
+                            <h2 className='text-main align-self-end'>...</h2>
+                            <p>{` ${message} : You`} </p>
+                            <img src={userIMG} className="m-1  align-self-end" width="40px" alt="" />
+                        </div>
+                        <p>10:34 PM</p>
+                    </div>
+                </div>
+                :
+                <div className="clientContainer">
+                    <div className="clientCard">
+                        <div className="clientBody">
+                            <img src={userAnna} className="m-1  align-self-end" width="40px" alt="" />
+                            <p>{`${user}: ${message}`} </p>
+                            <h2 className='text-main align-self-end'>...</h2>
+                        </div>
+                        <p>10:34 PM</p>
+                    </div>
+                </div>
+            }
+
+
+        </div>
+
+
+
+    );
+};
+
+export default Message;
